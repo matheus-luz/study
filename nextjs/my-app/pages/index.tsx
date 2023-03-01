3ba0cd65b17adc796480d8cf1859c2a653e163d4
matheus-luz <luz.matheus11@gmail.com>
2023-02-28 21:19:43 -0300

    feat: create route about one routes

---
+++ nextjs/my-app/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello world</h1>
+    </main>
+  )
+}
