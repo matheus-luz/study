0a767d6d8a4ec91db60858278f715209b012dc94
matheus-luz <luz.matheus11@gmail.com>
2023-03-11 20:03:48 -0300

    feat: :sparkles: create routes products and products/shirts

--- nextjs/my-app/pages/index.tsx
+++ nextjs/my-app/pages/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
       <h1>Hello world</h1>
+      <ul>
+        <li>
+          <Link href="/products">
+            Products
+          </Link>
+        </li>
+      </ul>
     </main>
   )
 }
